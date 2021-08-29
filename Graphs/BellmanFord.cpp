1b5873a003235c6c3327656fba241ad248df89d6
Vidit Bhardwaj <59336663+viditbhardwaj12@users.noreply.github.com>
2021-08-29 16:21:14 +0530

    Create BellmanFord.cpp

---
+++ Graphs/BellmanFord.cpp
@@ -0,0 +1,22 @@
+vector<int> dist(N);
+void bellman_ford(vector<vector<pii> > &l,int n){
+    int neg_cycle=0,relax=n,node=0;
+    fill(all(dist),inf);
+    dist[1]=0;
+    while(relax--){
+        neg_cycle=0;
+        for(int i=1;i<=n;i++){
+            for(auto x:l[i]){
+                int from=(i),to=x.ff,wt=x.ss;
+                if(dist[to]>dist[from]+wt){
+                    dist[to]=dist[from]+wt;
+                    neg_cycle=1,node=to;
+                } 
+            }
+        }
+    }
+
+    if(neg_cycle){
+        exit(0);   
+    }
+}
