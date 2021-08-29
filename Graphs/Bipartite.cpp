6edf388b500cc1272ecac0080ecfc16764985e6d
Vidit Bhardwaj <59336663+viditbhardwaj12@users.noreply.github.com>
2021-08-29 19:56:52 +0530

    Create Bipartite.cpp

---
+++ Graphs/Bipartite.cpp
@@ -0,0 +1,22 @@
+void dfs(int node,int parent,int clr,map<int,int> &color,int *odd_cycle,vector<int> &vis,vector<vector<int> > &l){
+    vis[node]=1;
+    color[node]=clr;
+    for(auto child:l[node]){
+        if(!vis[child]){
+            dfs(child,node,1-clr,color,odd_cycle,vis,l);
+        }else{
+            if(child!=parent && color[child]==clr) *odd_cycle=1;
+        }
+    }
+}
+bool bipartite(vector<vector<int> > &l,int n){
+    map<int,int> color;
+    int odd_cycle=0;
+    vector<int> vis(n+1,0);
+    for(int i=1;i<=n;i++){
+        if(!vis[i]){
+            dfs(i,0,1,color,&odd_cycle,vis,l);
+        }
+    }
+    return (odd_cycle);
+}
