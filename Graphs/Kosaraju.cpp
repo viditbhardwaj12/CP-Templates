98b8911671c34865afa0b0563d793518274f275b
Vidit Bhardwaj <59336663+viditbhardwaj12@users.noreply.github.com>
2021-08-30 12:16:20 +0530

    Create Kosaraju.cpp

---
+++ Graphs/Kosaraju.cpp
@@ -0,0 +1,42 @@
+void dfs_top(int prnt,vector<int> &path,vector<int> &vis,vector<vector<int> > &l){
+    vis[prnt]=1;
+    for(auto ch:l[prnt]){
+        if(!vis[ch]){
+            dfs_top(ch,path,vis,l);
+        }
+    }
+    path.pb(prnt);
+}
+vector<int> topsort(vector<vector<int> > &l,int n){
+    vector<int> path;
+    vector<int> vis(n+1,0);
+    for(int i=1;i<=n;i++){
+        if(!vis[i]){
+            dfs_top(i,path,vis,l);
+        }
+    }
+    reverse(all(path));
+    return path;
+}
+void dfs(int prnt,vector<int> &comp,vector<int> &vis,vector<vector<int> > &rev_l){
+    vis[prnt]=1,comp.pb(prnt);
+    for(auto ch:rev_l[prnt]){
+        if(!vis[ch]){
+            dfs(ch,comp,vis,rev_l);
+        }
+    }
+}
+vector<vector<int> > kosaraju(int n,vector<vector<int> > &l,vector<vector<int> >  &rev_l){
+    vector<int> path=topsort(l,n);
+
+    vector<int> vis(n+1,0);
+    vector<vector<int> > scc;
+    for(int i=0;i<sz(path);i++){
+        if(!vis[path[i]]){
+            vector<int> comp;
+            dfs(path[i],comp,vis,rev_l);
+            scc.pb(comp);
+        }
+    }
+    return scc;
+}
