41021b12a0ad253783771953ba67003dba6c5faf
Vidit Bhardwaj <59336663+viditbhardwaj12@users.noreply.github.com>
2021-08-31 12:35:32 +0530

    Create InverseMod.cpp

---
+++ NumberTh/InverseMod.cpp
@@ -0,0 +1,9 @@
+int pow(int a,int b,int m){
+    int ans=1;
+    while(b){
+        if(b%2) ans=(ans*a)%m;
+        b/=2,a=(a*a)%m;
+    }
+    return ans;
+}
+int inv_mod(int a,int m){return pow(a,m-2,m);}
