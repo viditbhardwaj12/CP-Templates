61f9e905ce05df992bd2677b27c9172460d74498
Vidit Bhardwaj <59336663+viditbhardwaj12@users.noreply.github.com>
2021-08-31 12:38:42 +0530

    Create PrimeSieve.cpp

---
+++ NumberTh/PrimeSieve.cpp
@@ -0,0 +1,13 @@
+vector<int> sieve(int n){ 
+    vector<int> p;
+    bool prime[n+1];  
+    mset(prime,true);     
+    prime[0]=prime[1]=false;
+    for(int i=2;i<=n;i++){
+        if(prime[i]){
+            for(int j=i*i;j<=n;j+=i) prime[j]=false;
+            p.pb(i);
+        }
+    }
+    return p;
+}
