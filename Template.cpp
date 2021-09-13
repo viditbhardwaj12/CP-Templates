c1d3550deca3d41d46e67eef104731ab084713b8
Vidit Bhardwaj <59336663+viditbhardwaj12@users.noreply.github.com>
2021-09-13 15:33:35 +0530

    Update Template.cpp

--- Template.cpp
+++ Template.cpp
@@ -16,13 +16,13 @@ using namespace __gnu_pbds;
 #define ub               upper_bound
 #define int              long long int
 #define double           long double
-#define TC               int TT;cin>>TT;while(TT--)
+#define TC               int TT; cin>>TT; while(TT--)
 #define sz(x)            (int)(x.size()) 
 #define mset(a,b)        memset(a,b,sizeof(a))
 #define all(v)           (v).begin(),(v).end()
 #define zerobits(x)      __builtin_ctzll(x)
 #define setbits(x)       __builtin_popcountll(x)
-#define SPD_emergency    ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define SPD_emergency    ios_base::sync_with_stdio(false); cin.tie(NULL);
 #define ps(x,y)          fixed<<setprecision(y)<<x
 #define YES              cout<<"YES"<<endl
 #define NO               cout<<"NO"<<endl
@@ -34,7 +34,7 @@ template<typename T> using max_pq=priority_queue<T>;
 typedef pair<int,int> pii;
 
 #ifndef ONLINE_JUDGE
-#define debug(x) cerr<<"["<<#x<<"]"<<"->";_print(x);cerr<<endl;
+#define debug(x) cerr<<"["<<#x<<"]"<<"->"; _print(x); cerr<<endl;
 #else
 #define debug(x)
 #endif
@@ -49,18 +49,18 @@ template<class T> void _print(vector<T> T2);
 template<class T> void _print(set<T> s);
 template<class T> void _print(multiset<T> s);
 template<class T1,class T2> void _print(map<T1,T2> mp);
-template<class T> void _print(vector<T> T2){int c=0;cerr<<"[";for(T x:T2){cerr<<(c++?",":""),_print(x);}cerr<<"]";}
-template<class T> void _print(set<T> s){int c=0;cerr<<"[";for(T x:s){cerr<<(c++?",":""),_print(x);}cerr<<"]";}
-template<class T> void _print(multiset<T> s){int c=0;cerr<<"[";for(T x:s){cerr<<(c++?",":""),_print(x);}cerr << "]";}
-template<class T1,class T2> void _print(pair<T1,T2> p){cerr<<"{";_print(p.ff);cerr<<",";_print(p.ss);cerr<<"}";}
-template<class T1,class T2> void _print(map<T1,T2> mp){int c=0;cerr<<"[";for(auto x:mp){cerr<<(c++?",":""),_print(x);}cerr<<"]";}
-void _print(pbds<int> s){int c=0;cerr<<"[";for(auto x:s){cerr<<(c++?",":""),_print(x);}cerr<<"]";}
+template<class T> void _print(vector<T> T2){int c=0; cerr<<"["; for(T x:T2){cerr<<(c++?",":""),_print(x);}cerr<<"]";}
+template<class T> void _print(set<T> s){int c=0; cerr<<"["; for(T x:s){cerr<<(c++?",":""),_print(x);}cerr<<"]";}
+template<class T> void _print(multiset<T> s){int c=0; cerr<<"["; for(T x:s){cerr<<(c++?",":""),_print(x);}cerr << "]";}
+template<class T1,class T2> void _print(pair<T1,T2> p){cerr<<"{"; _print(p.ff); cerr<<","; _print(p.ss); cerr<<"}";}
+template<class T1,class T2> void _print(map<T1,T2> mp){int c=0; cerr<<"["; for(auto x:mp){cerr<<(c++?",":""),_print(x);}cerr<<"]";}
+void _print(pbds<int> s){int c=0; cerr<<"["; for(auto x:s){cerr<<(c++?",":""),_print(x);}cerr<<"]";}
 
 const int N=(2e5+10),mod=(1e9+7),inf=(1e18);
 const double pi=3.14159265358979323846264338;
 
-int _pow(int a,int b){if(!b){return 1;}int ans=_pow(a,b/2);ans*=ans;if(b%2){return ans*=a;}return ans;}
-int gcd(int a,int b){return b?gcd(b,a%b):a;} int lcm(int a,int b){return a/gcd(a,b)*b;}
+int _pow(int a,int b){if(!b){return 1;}int ans=_pow(a,b/2); ans*=ans; if(b%2){return ans*=a;}return ans;}
+int gcd(int a,int b){return b?gcd(b,a%b):a;}int lcm(int a,int b){return a/gcd(a,b)*b;}
 /*____________________________________________________________________*/
 
 signed main(){
@@ -70,7 +70,7 @@ SPD_emergency
 #endif
 
 TC{
-        
+    
 }
 
     return 0;
