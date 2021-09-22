dd5a755290c85bd3e8ba910b5990e2ba494dd110
Vidit Bhardwaj <59336663+viditbhardwaj12@users.noreply.github.com>
2021-09-22 13:01:55 +0530

    Update CyclicDetectionUndir.cpp

--- Graphs/CyclicDetectionUndir.cpp
+++ Graphs/CyclicDetectionUndir.cpp
@@ -1,10 +1,33 @@
-bool dfs(int node,int parent,vector<vector<int> > &l,vector<int> vis){
+bool dfs(int node,int parent,vector<vector<int> > &l,vector<int> &vis){
     vis[node]=1;
     for(auto child:l[node]){
         if(!vis[child]){
             if(dfs(child,node,l,vis)) return true;
         }else{
             if(child!=parent) return true;  
+        }
+    }
+    return false;
+}
+
+// if (edges>=nodes)
+
+void dfs(int i,int &edges,int &nodes,vector<int> &vis,vector<vector<int> > &l){
+    vis[i]=1,nodes++;
+    edges+=l[i].size();
+    for(auto j:l[i]){
+        if(!vis[j])
+            dfs(j,edges,nodes,vis,l);
+    }
+}
+int is_cyclic(int n,vector<vector<int> > &l){
+    vector<int> vis(n+1,0);
+    for(int i=1;i<=n;i++){
+        if(!vis[i]){
+            int edges=0,nodes=0;
+            dfs(i,edges,nodes,vis,l);
+            if(edges/2>=nodes)
+                return true;
         }
     }
     return false;
