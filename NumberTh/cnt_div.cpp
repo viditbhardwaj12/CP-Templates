4885d7e96eb9b86cba12e758fe71ce9fc0e0bcac
Vidit Bhardwaj <59336663+viditbhardwaj12@users.noreply.github.com>
2021-09-22 11:50:41 +0530

    Update cnt_div.cpp

--- NumberTh/cnt_div.cpp
+++ NumberTh/cnt_div.cpp
@@ -1,11 +1,25 @@
-// This will give count of all divisors till 'n'
 // nlog(n)
 
+// This will give count of all divisors till 'n'
 vector<int> cnt_div(int n){
     vector<int> cnt(n+1,0);
     for(int i=1;i<=n;i++){
         for(int j=i;j<=n;j+=i){
             cnt[j]++;
+        }
+    }
+    return cnt;
+}
+
+// This will give count of all prime divisors till 'n'
+
+vector<int> cnt_prime_div(int n){
+    vector<int> cnt(n+1,0),vis(n+1,0);
+    for(int i=2;i<=n;i++){
+        if(!vis[i]){
+            for(int j=i;j<=n;j+=i){
+                cnt[j]++,vis[j]=1;
+            }
         }
     }
     return cnt;
