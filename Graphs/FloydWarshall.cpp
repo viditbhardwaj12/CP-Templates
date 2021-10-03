9325ae6028c8a170c54fbcb4dcecfcd84ac33b65
Vidit Bhardwaj <59336663+viditbhardwaj12@users.noreply.github.com>
2021-10-03 15:45:56 +0530

    Update FloydWarshall.cpp

--- Graphs/FloydWarshall.cpp
+++ Graphs/FloydWarshall.cpp
@@ -1,54 +1,38 @@
-// By : ViditBhardwaj
-#include<bits/stdc++.h>
-using namespace std;
- 
-#define endl             "\n"
-#define int              long long int
-#define all(v)           (v).begin(),(v).end()
-#define SPD_emergency    ios_base::sync_with_stdio(false);cin.tie(NULL);
- 
-const int N=(5e2+10),inf=(1e18);
+// Dir or undir
+// Gives the shortest path between each pair of vertices i and j.
+// The graph may have negative weight edges, but no negative weight cycles.
+// Undir graph cannot have any -ve wt edges,such edge forms already a -ve cycle as we can move back and forth along that edge as long as you like.(Shortest path is undefined)
+// T=O(n^3)
 
-int n,m;
-vector<vector<int> > dist(N,vector<int>(N));
-void floyd_warshall(){
-    for(int mid=0;mid<n;mid++){
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                dist[i][j]=min(dist[i][j],dist[i][mid]+dist[mid][j]);
-            }
-        }
-    }
-}
- 
-signed main(){
-SPD_emergency
-    
-    int q;
-    cin>>n>>m>>q;
-    for(int i=0;i<=n;i++){
+// if(dist[i][j]== inf) shortest path from 'i' to 'j' doesn't exists
+// if(dist[i][j]== -ve) -ve wt cycle exists(detects -ve cycle)
+
+// Line 32: if the graph has -ve wt edges,it is better to it in the following way,so that it does not perform transitions using paths that don't exist.
+
+// Maintain p[][] for getting path.Recursively find path between i and p[i][j], p[i][j] and j.
+
+// Remember nodes exists from 0 to (n-1)
+vector<vector<int> > floyd_warshall(vector<vector<int> > &l,int n){
+    vector<vector<int> > dist(n,vector<int>(n));
+    for(int i=0;i<n;i++){
         fill(all(dist[i]),inf);
         dist[i][i]=0;
     }
- 
-    for(int i=0;i<m;i++){
-        int x,y,wt; cin>>x>>y>>wt;
-        x--,y--;
-        dist[x][y]=min(dist[x][y],wt);
-        dist[y][x]=min(dist[y][x],wt);
+
+    for(auto &e:l){
+        int x=e[0],y=e[1],w=e[2];
+        dist[x][y]=min(dist[x][y],w);
+        dist[y][x]=min(dist[y][x],w);
     }
- 
-    floyd_warshall();
- 
-    while(q--){
-        int x,y; cin>>x>>y;
-        x--,y--;
-        if(dist[x][y]==inf) cout<<"-1"<<endl;
-        else cout<<dist[x][y]<<endl;
+
+    for(int mid=0;mid<n;mid++){
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(dist[i][mid]<inf && dist[mid][j]<inf){
+                    dist[i][j]=min(dist[i][j],dist[i][mid]+dist[mid][j]);
+                }
+            }
+        }
     }
- 
-    return 0;
+    return dist;
 }
-/*
- 
-*/
