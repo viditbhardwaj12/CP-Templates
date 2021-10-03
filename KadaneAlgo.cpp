2307669bb84d44b028b02f59831eeda49464e150
Vidit Bhardwaj <59336663+viditbhardwaj12@users.noreply.github.com>
2021-10-03 13:06:47 +0530

    Update KadaneAlgo.cpp

--- KadaneAlgo.cpp
+++ KadaneAlgo.cpp
@@ -1,25 +1,9 @@
-#include<bits/stdc++.h>
-using namespace std;
-
-#define int              long long int
-
-const int inf=(1e18);
-
-signed main(){
-
-    int n; cin>>n;
-    vector<int> a(n);
-    for(int i=0;i<n;i++){
-        cin>>a[i];
-    }
-
-    int curr_max=0,ans=-inf;
+// Max Subarray Sum
+int kadaneAlgo(vector<int> &a){
+    int curr_max=0,ans=-inf,n=sz(a);
     for(int i=0;i<n;i++){
         curr_max=max(a[i],curr_max+a[i]);
         ans=max(ans,curr_max);
     }
-
-    cout<<ans;
-
-    return 0;
+    return ans;
 }
