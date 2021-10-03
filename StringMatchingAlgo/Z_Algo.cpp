133f160ab839ab4c7a98cd29729d4335aab8994b
Vidit Bhardwaj <59336663+viditbhardwaj12@users.noreply.github.com>
2021-10-03 16:37:48 +0530

    Update Z_Algo.cpp

--- StringMatchingAlgo/Z_Algo.cpp
+++ StringMatchingAlgo/Z_Algo.cpp
@@ -1,3 +1,8 @@
+// Z[i]=Length of longest subtring starting at ith indx which also is prefix of string
+// a b a a b a b a b a a b a b a
+// 0 0 1 3 0 3 0 8 0 1 3 0 3 0 1
+// T=O(N)
+
 vector<int> z_algo(string str){
     int n=sz(str);
     vector<int> z(n);
