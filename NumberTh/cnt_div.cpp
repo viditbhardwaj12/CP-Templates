885cdbe19d628bc5b40b5d6f8a6eb5c503ac6bd8
Vidit Bhardwaj <59336663+viditbhardwaj12@users.noreply.github.com>
2021-10-14 22:55:21 +0530

    Update cnt_div.cpp

--- NumberTh/cnt_div.cpp
+++ NumberTh/cnt_div.cpp
@@ -24,28 +24,3 @@ vector<int> cnt_prime_div(int n){
     }
     return cnt;
 }
-  
-// This will give prime factorization of 'n'
-// 0(sqrt(n)/log(n))
-// count of prime numbers is from 1 to n => (n/log(n))
-vector<int> prime_fact(int n){
-    vector<int> p,unique_p;
-    if(n%2==0){
-        unique_p.pb(2);
-        while(n%2==0){
-            p.pb(2),n/=2;
-        }
-    }
-
-    for(int i=3;i*i<=n;i+=2){
-        if(n%i==0){
-            unique_p.pb(i);
-            while(n%i==0){
-                n/=i,p.pb(i);
-            }
-        }
-    }
-    if(n>1) p.pb(n),unique_p.pb(n);
-//     return unique_p;
-    return p;
-}
