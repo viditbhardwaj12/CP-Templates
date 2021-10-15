e49392d109e8d80c88abb3f1b5799e6c3c9535b9
Vidit Bhardwaj <59336663+viditbhardwaj12@users.noreply.github.com>
2021-10-15 15:02:32 +0530

    Update InverseModExtended[Extended Euclidean gcd].cpp

--- NumberTh/InverseModExtended[Extended Euclidean gcd].cpp
+++ NumberTh/InverseModExtended[Extended Euclidean gcd].cpp
@@ -1,7 +1,8 @@
 // LINEAR DIOPHANTINE EQ;
-// ax+by=g
+// solves: ax+by=g (a & b are not co-prime) & ax+by=1 (a & b are co-prime)    
 // x=> multiplicative modular inverse when 'm' & 'a' are not co-prime with one another
 // g=> gcd(a,b)
+// T=log(min(a,b))
 
 int extended_gcd(int a,int b,int& x,int& y){
     if(b==0){x=1,y=0; return a;}
