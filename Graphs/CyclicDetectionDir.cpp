635b11703878ce764242cee27eb9fa6246173342
Vidit Bhardwaj <59336663+viditbhardwaj12@users.noreply.github.com>
2021-10-20 13:47:17 +0530

    Update CyclicDetectionDir.cpp

--- Graphs/CyclicDetectionDir.cpp
+++ Graphs/CyclicDetectionDir.cpp
@@ -1,4 +1,4 @@
-bool dfs(int node,map<int,int> &curr_path,vector<vector<int> > &l,vector<int> &vis){
+bool dfs(int node,vector<int> &curr_path,vector<vector<int> > &l,vector<int> &vis){
     vis[node]=1;
     curr_path[node]=1;
     for(auto child:l[node]){
@@ -12,8 +12,7 @@ bool dfs(int node,map<int,int> &curr_path,vector<vector<int> > &l,vector<int> &v
     return false;        
 }
 bool cyclic(vector<vector<int> > &l,int n){
-    map<int,int> curr_path;
-    vector<int> vis(n+1,0);
+    vector<int> vis(n+1,0),curr_path(n+1,0);
     for(int i=1;i<=n;i++){
         if(!vis[i] && dfs(i,curr_path,l,vis))
             return true;
