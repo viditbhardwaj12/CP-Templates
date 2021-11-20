a1547e91b2241085f0ed158f7a18465bf8b4c711
Vidit Bhardwaj <59336663+viditbhardwaj12@users.noreply.github.com>
2021-11-20 12:53:44 +0530

    Update Template.cpp

--- Template.cpp
+++ Template.cpp
@@ -44,28 +44,17 @@ void _print(double x){cerr<<x;}
 void _print(char x){cerr<<x;}
 void _print(string x){cerr<<x;}
 
-template<class T> void _print(vector<T> v);
-template<class T> void _print(deque<T> dq);
-template<class T> void _print(set<T> s);
-template<class T> void _print(multiset<T> s);
-template<class T> void _print(queue<T> q);
-template<class T> void _print(min_pq<T> q);
-template<class T> void _print(max_pq<T> q);
-template<class T> void _print(stack<T> st);
-template<class T1,class T2> void _print(pair<T1,T2> p);
-template<class T1,class T2> void _print(map<T1,T2> mp);
-template<class T> void _print(pbds<T> s);
-template<class T> void _print(vector<T> v){int c=0; cerr<<"["; for(T x:v){cerr<<(c++?",":""),_print(x);}cerr<<"]";}
-template<class T> void _print(deque<T> dq){int c=0; cerr<<"["; for(T x:dq){cerr<<(c++?",":""),_print(x);}cerr<<"]";}
-template<class T> void _print(set<T> s){int c=0; cerr<<"["; for(T x:s){cerr<<(c++?",":""),_print(x);}cerr<<"]";}
-template<class T> void _print(multiset<T> s){int c=0; cerr<<"["; for(T x:s){cerr<<(c++?",":""),_print(x);}cerr << "]";}
-template<class T> void _print(queue<T> q){int c=0; cerr<<"["; while(!q.empty()){cerr<<(c++?",":""),_print(q.front()),q.pop();}cerr<<"]";}
-template<class T> void _print(min_pq<T> q){int c=0; cerr<<"["; while(!q.empty()){cerr<<(c++?",":""),_print(q.top()),q.pop();}cerr<<"]";}
-template<class T> void _print(max_pq<T> q){int c=0; cerr<<"["; while(!q.empty()){cerr<<(c++?",":""),_print(q.top()),q.pop();}cerr<<"]";}
-template<class T> void _print(stack<T> st){int c=0; cerr<<"["; while(!st.empty()){cerr<<(c++?",":""),_print(st.top()),st.pop();}cerr<<"]";}
-template<class T1,class T2> void _print(pair<T1,T2> p){cerr<<"{"; _print(p.ff); cerr<<","; _print(p.ss); cerr<<"}";}
-template<class T1,class T2> void _print(map<T1,T2> mp){int c=0; cerr<<"["; for(auto x:mp){cerr<<(c++?",":""),_print(x);}cerr<<"]";}
-void _print(pbds<int> s){int c=0; cerr<<"["; for(auto x:s){cerr<<(c++?",":""),_print(x);}cerr<<"]";}
+template<class T> void _print(vector<T> v); template<class T> void _print(vector<T> v){int c=0; cerr<<"["; for(T x:v){cerr<<(c++?",":""),_print(x);}cerr<<"]";}
+template<class T> void _print(deque<T> dq); template<class T> void _print(deque<T> dq){int c=0; cerr<<"["; for(T x:dq){cerr<<(c++?",":""),_print(x);}cerr<<"]";}
+template<class T> void _print(set<T> s); template<class T> void _print(set<T> s){int c=0; cerr<<"["; for(T x:s){cerr<<(c++?",":""),_print(x);}cerr<<"]";}
+template<class T> void _print(multiset<T> s); template<class T> void _print(multiset<T> s){int c=0; cerr<<"["; for(T x:s){cerr<<(c++?",":""),_print(x);}cerr << "]";}
+template<class T> void _print(queue<T> q); template<class T> void _print(queue<T> q){int c=0; cerr<<"["; while(!q.empty()){cerr<<(c++?",":""),_print(q.front()),q.pop();}cerr<<"]";}
+template<class T> void _print(min_pq<T> q); template<class T> void _print(min_pq<T> q){int c=0; cerr<<"["; while(!q.empty()){cerr<<(c++?",":""),_print(q.top()),q.pop();}cerr<<"]";}
+template<class T> void _print(max_pq<T> q); template<class T> void _print(max_pq<T> q){int c=0; cerr<<"["; while(!q.empty()){cerr<<(c++?",":""),_print(q.top()),q.pop();}cerr<<"]";}
+template<class T> void _print(stack<T> st); template<class T> void _print(stack<T> st){int c=0; cerr<<"["; while(!st.empty()){cerr<<(c++?",":""),_print(st.top()),st.pop();}cerr<<"]";}
+template<class T1,class T2> void _print(pair<T1,T2> p); template<class T1,class T2> void _print(pair<T1,T2> p){cerr<<"{"; _print(p.ff); cerr<<","; _print(p.ss); cerr<<"}";}
+template<class T1,class T2> void _print(map<T1,T2> mp); template<class T1,class T2> void _print(map<T1,T2> mp){int c=0; cerr<<"["; for(auto x:mp){cerr<<(c++?",":""),_print(x);}cerr<<"]";}
+template<class T> void _print(pbds<T> s); void _print(pbds<int> s){int c=0; cerr<<"["; for(auto x:s){cerr<<(c++?",":""),_print(x);}cerr<<"]";}
 
 const int N=(2e5+10),mod=(1e9+7),inf=(1e18);
 const double pi=3.14159265358979323846264338;
@@ -81,7 +70,7 @@ SPD_emergency
 #endif
 
 TC{
-
+    
 }
 
     return 0;
