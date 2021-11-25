7a9e0858e164f6def44cedeb2e83a9edda593bbf
Vidit Bhardwaj <59336663+viditbhardwaj12@users.noreply.github.com>
2021-11-25 17:34:57 +0530

    Update and rename #CatalanNumber.cpp to CatalanNumber.cpp

---
+++ Dp/CatalanNumber.cpp
@@ -0,0 +1,25 @@
+int catalan(int n){
+    vector<int> dp(n+1,0);
+    dp[0]=1,dp[1]=1;
+    for(int i=2;i<=n;i++){
+        for(int j=0;j<i;j++){
+            dp[i]+=(dp[j]*dp[i-j-1]);
+        }
+    }
+    return dp[n];
+}
+
+int CatalanRec(int n,vector<int> &dp){
+    if(n==0 || n==1){
+        return dp[n]=1;
+    }
+    if(dp[n]!=-1){
+        return dp[n];
+    }
+
+    int ans=0;
+    for(int i=1;i<=n;i++){
+        ans+=CatalanRec(n-i,dp)*CatalanRec(i-1,dp);
+    }
+    return dp[n]=ans;
+}
