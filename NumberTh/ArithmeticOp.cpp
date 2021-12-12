5788ca0bb111e37df73bff00fcaa031f196cad24
Vidit Bhardwaj <59336663+viditbhardwaj12@users.noreply.github.com>
2021-12-12 11:16:24 +0530

    Update ArithmeticOp.cpp

--- NumberTh/ArithmeticOp.cpp
+++ NumberTh/ArithmeticOp.cpp
@@ -1,8 +1,8 @@
-int add(int a,int b,int m){return (a+b)%m;}
+int add_mod(int a,int b,int m){return (a+b)%m;}
 
-int sub(int a,int b,int m){return ((a-b)%m+m)%m;}
+int sub_mod(int a,int b,int m){return ((a-b)%m+m)%m;}
 
-int mul(int a,int b,int m){return ((a%m)*(b%m))%m;}
+int mul_mod(int a,int b,int m){return ((a%m)*(b%m))%m;}
 
 int pow(int a,int b,int m){
     int ans=1;
@@ -13,4 +13,4 @@ int pow(int a,int b,int m){
     return ans;
 }
 int inv_mod(int a,int m){return pow(a,m-2,m);}
-int div(int a,int b,int m){return (a*inv_mod(b,m))%m;}
+int div_mod(int a,int b,int m){return (a*inv_mod(b,m))%m;}
