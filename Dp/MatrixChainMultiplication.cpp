0bac83b10afa45f50e11c4148ab66a592483bbc9
Vidit Bhardwaj <59336663+viditbhardwaj12@users.noreply.github.com>
2022-01-19 12:17:07 +0530

    Update MatrixChainMultiplication.cpp

--- Dp/MatrixChainMultiplication.cpp
+++ Dp/MatrixChainMultiplication.cpp
@@ -1,31 +1,15 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-#define ff               first
-#define ss               second
-#define endl             "\n"
-#define pb               push_back
-#define popb             pop_back
-#define int              long long int
-#define sz(x)            (int)(x.size()) 
-#define mset(a,b)        memset(a,b,sizeof(a))
-#define all(v)           (v).begin(),(v).end()
-#define SPD_emergency    ios_base::sync_with_stdio(false); cin.tie(NULL);
-#define TC               int TT; cin>>TT; while(TT--)
-
-const int N=(2e5+10),mod=(1e9+7),inf=(1e18);
-
 int mcm(vector<int> &a){
-    int n=sz(a)-1;
-    vector<vector<int> > dp(n,vector<int>(n));
+    int n=a.size()-1;
+    vector<vector<int> > dp(n,vector<int>(n,0));
     for(int i=n-1;i>=0;i--){
-        for(int j=0;j<n;j++){
-            if(i>=j){
-                dp[i][j]=0;
-            }else if((j-i)==1){
+        for(int j=i+1;j<n;j++){
+            if((j-i)==1){
                 dp[i][j]=a[i]*a[j]*a[j+1];
             }else{
-                dp[i][j]=inf;
+                dp[i][j]=INT_MAX;
                 for(int k=i;k<j;k++){
                     dp[i][j]=min(dp[i][j],dp[i][k]+dp[k+1][j]+a[i]*a[k+1]*a[j+1]);
                 }
@@ -36,9 +20,7 @@ int mcm(vector<int> &a){
 }
 
 signed main(){
-SPD_emergency
 
-TC{
     int n; cin>>n;
     vector<int> a(n);
     for(int i=0;i<n;i++){
@@ -46,7 +28,11 @@ TC{
     }
 
     cout<<mcm(a)<<endl;
-}
 
     return 0;
 }
+/*
+    (ABC)D = (AB)(CD) = A(BCD) = ....
+    (AB)C = (10×30×5) + (10×5×60) = 1500 + 3000 = 4500 operations
+    A(BC) = (30×5×60) + (10×30×60) = 9000 + 18000 = 27000 operations.
+*/
