6aa88842447216c1b2990a5973c0903facfd05ce
Vidit Bhardwaj <59336663+viditbhardwaj12@users.noreply.github.com>
2022-01-19 11:57:39 +0530

    Update CatalanNumber.cpp

--- Dp/CatalanNumber.cpp
+++ Dp/CatalanNumber.cpp
@@ -1,4 +1,11 @@
 // Count Number of Binary Search Trees with N Nodes Dynamic Programming - Catalan Numbers
+// C0=1
+// C1=1
+// C2=C0*C1 + C1*C0
+// C3=C0*C2 + C1*C1 + C2*C0
+// C4=C0*C3 + C1*C2 + C2*C1 + C3*C0
+// C5=C0*C4 + C1*C3 + C2*C2 + C3*C1 + C4*C0
+
 int catalan(int n){
     vector<int> dp(n+1,0);
     dp[0]=1,dp[1]=1;
