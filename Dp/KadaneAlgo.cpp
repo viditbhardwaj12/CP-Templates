09e68bfae27dfde59591b2bdf4d661fdc884e5da
Vidit Bhardwaj <59336663+viditbhardwaj12@users.noreply.github.com>
2022-02-23 18:37:45 +0530

    Delete Dp directory

--- Dp/KadaneAlgo.cpp
+++
@@ -1,9 +0,0 @@
-// Max Subarray Sum
-int kadaneAlgo(vector<int> &a){
-    int curr_max=a[0],ans=a[0];
-    for(int i=1;i<a.size();i++){
-        curr_max=max(a[i],curr_max+a[i]);
-        ans=max(ans,curr_max);
-    }
-    return ans;
-}
--- Dp/LongestIncSubseq.cpp
+++
@@ -1,9 +0,0 @@
-int lis(vector<int> &a,int n){
-    vector<int> v;
-    for(int i=0;i<n;i++){
-        int j=lb(all(v),a[i])-v.begin();
-        if(j<sz(v)) v[j]=a[i];
-        else v.pb(a[i]);
-    }
-    return sz(v);
-}
